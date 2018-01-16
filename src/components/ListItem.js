017b1bc65ab069b0825767620c01f814051d013d
ziwer1 <ziwer1@gmail.com>
2018-01-16 23:10:42 +0300

    Add renderListItem function

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -12,11 +12,17 @@ export default class ListItem extends React.Component{
 		super(props);
 	}
 
+	renderListItem(){
+		if (this.props.item.completed == false){
+			return <ListGroupItem>&nbsp; <Input type="checkbox" className="float-right" /> &nbsp; {this.props.item.item}&nbsp; {this.props.id}</ListGroupItem>
+		}else{
+			return <ListGroupItem>&nbsp; <Input type="checkbox" className="float-right" checked /> &nbsp; <s>{this.props.item.item}&nbsp; {this.props.id}</s></ListGroupItem>
+		}
+	}
+
 	render(){
 		{/*<ListGroupItem onClick={this.props.clickHandler}>{this.props.children}</ListGroupItem>*/}
-		return(
-			<ListGroupItem>&nbsp; <Input type="checkbox" className="float-right" /> &nbsp; {this.props.children}</ListGroupItem>
-		);
+		return this.renderListItem()
 	}
 
 
